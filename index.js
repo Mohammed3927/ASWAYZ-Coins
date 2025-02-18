0c197e3d67ddb2e51627e820ea69178cb018a4f3
Mohammed3927 <gkhal839@gmail.com>
2025-02-18 21:54:17 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const config = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+client.commands = new Map();
+
+// Load all commands from `commands/` folder
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.on('messageCreate', async message => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (client.commands.has(commandName)) {
+        client.commands.get(commandName).execute(message, args);
+    }
+});
+
+client.login(config.token);
